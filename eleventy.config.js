5625213b008f5a827a475623212145ee823da7c4
Martin Berglund <martin.berglund@nav.no>
2022-11-30 09:26:14 +0100

    Update dependencies and config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,27 +1,67 @@
-const compress = require('compression');
+const markdownIt = require('markdown-it')
+const htmlmin = require('html-minifier')
+const compression = require('compression')
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "./node_modules/alpinejs/dist/cdn.min.js": "./main.min.js" });
-  eleventyConfig.addPassthroughCopy("./static/images/*");
-  eleventyConfig.addPassthroughCopy("./static/fonts/*");
-  eleventyConfig.addWatchTarget("./src/main.css");
-  eleventyConfig.setDataDeepMerge(true);
-  eleventyConfig.setBrowserSyncConfig({
-    server: {
-      baseDir: './dist',
-      middleware: function(req,res,next){
-        const gzip = compress();
-        gzip(req,res,next);
-      }
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    './node_modules/alpinejs/dist/cdn.min.js': './main.min.js',
+  })
+  eleventyConfig.addPassthroughCopy('./src/assets/images/*')
+  eleventyConfig.addPassthroughCopy('./src/assets/fonts/*')
+  eleventyConfig.addWatchTarget('./src/assets/main.css')
+  eleventyConfig.setDataDeepMerge(true)
+
+  eleventyConfig.addFilter('markdown', function (value) {
+    return new markdownIt({
+      html: true,
+      typographer: true,
+      quotes: '«»',
+    }).render(value)
+  })
+
+  eleventyConfig.addFilter('markdownInline', function (value) {
+    return new markdownIt({
+      html: false,
+    }).renderInline(value)
+  })
+
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath && outputPath.endsWith('.html')) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: {
+          level: 2,
+        },
+      })
+      return minified
     }
-  });
+
+    return content
+  })
+
+  eleventyConfig.setServerOptions({
+    enabled: true,
+    showVersion: true,
+    port: 8888,
+    middleware: [
+      // function compression(req, res, next) {
+      //   res.writeHead(200, { "content-type": "text/html" });
+      //   res.end();
+      //   next();
+      // },
+    ],
+  })
 
   return {
+    markdownTemplateEngine: 'njk',
+    templateFormats: ['njk', 'md'],
     dir: {
-      input: "src",
-      data: "_data",
-      includes: "_includes",
-      output: "dist",
-    }
+      input: 'src',
+      data: '_data',
+      includes: '_includes',
+      output: 'dist',
+    },
   }
 }
