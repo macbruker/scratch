20e12addca833a59d03f59d6a3e66526af12d323
Martin Berglund <martin.berglund@nav.no>
2025-11-20 11:27:42 +0100

    Simplify setup

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,25 @@
-const markdownIt = require('markdown-it')
-const htmlmin = require('html-minifier-terser')
-const compression = require('compression')
+import markdownIt from 'markdown-it'
+import htmlMinifier from 'html-minifier-terser'
+import compression from 'compression'
+import { transform } from 'lightningcss'
+import { EleventyHtmlBasePlugin } from '@11ty/eleventy'
 
-module.exports = function (eleventyConfig) {
+export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
   eleventyConfig.addPassthroughCopy({
     './node_modules/alpinejs/dist/cdn.min.js': './main.min.js',
   })
   eleventyConfig.addPassthroughCopy('./src/assets/images/*')
   eleventyConfig.addPassthroughCopy('./src/assets/fonts/*')
   eleventyConfig.addWatchTarget('./src/assets/main.css')
-  eleventyConfig.setDataDeepMerge(true)
+
+  eleventyConfig.setLibrary(
+    'md',
+    markdownIt({
+      html: true,
+      breaks: false,
+    }),
+  )
 
   eleventyConfig.addFilter('markdown', function (value) {
     return new markdownIt({
@@ -26,8 +36,9 @@ module.exports = function (eleventyConfig) {
   })
 
   eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
-    if (outputPath && outputPath.endsWith('.html')) {
-      let minified = htmlmin.minify(content, {
+    const env = process.env.ELEVENTY_ENV
+    if (env === 'production' && outputPath && outputPath.endsWith('.html')) {
+      let minified = htmlMinifier.minify(content, {
         useShortDoctype: true,
         removeComments: true,
         collapseWhitespace: true,
@@ -41,10 +52,44 @@ module.exports = function (eleventyConfig) {
     return content
   })
 
+  // Build Time
+  eleventyConfig.addGlobalData('generated', () => {
+    let now = new Date()
+    return new Intl.DateTimeFormat('no-NB', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    }).format(now)
+  })
+
+  // Bundles
+  eleventyConfig.addBundle('css', {
+    toFileDirectory: '',
+    transforms: [
+      async function (content) {
+        // 'this.type' returns the bundle name.
+        if (this.type === 'css') {
+          let result = await transform({
+            code: Buffer.from(content),
+            minify: true,
+            sourceMap: true,
+            drafts: {
+              nesting: true,
+            },
+          })
+          return result.code
+        }
+        return content
+      },
+    ],
+  })
+
+  eleventyConfig.setDataDeepMerge(true)
+
   eleventyConfig.setServerOptions({
     enabled: true,
     showVersion: true,
-    port: 8888,
+    // port: 8888,
     middleware: [
       // function compression(req, res, next) {
       //   res.writeHead(200, { "content-type": "text/html" });
--- src/_data/site.js
+++ src/_data/site.js
@@ -1,3 +1,3 @@
-module.exports = {
-  title: 'Scratch'
+export default {
+  title: 'Scratch',
 }
